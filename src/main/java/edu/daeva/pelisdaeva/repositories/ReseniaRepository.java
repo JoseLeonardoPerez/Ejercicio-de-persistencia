84850a4b8273c90c486eb2f70fd165858d1de997
nachovilla123 <nacho.villarruel@gmail.com>
2024-10-20 18:25:03 -0300

    borro repos

--- src/main/java/edu/daeva/pelisdaeva/repositories/ReseniaRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.daeva.pelisdaeva.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReseniaRepository extends JpaRepository<Resenia, Long> {
-
-}
